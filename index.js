ded40e9a3bbb48eee143a5c3461034ccc287b13b
mohammad esmaiel Emadi <mohesem@gmail.com>
2020-04-25 18:35:14 +0430

    with express

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import express from "express";
-
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 import Cors from "cors";
 import path from "path";
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(Cors());
 app.use(express.static(path.resolve("build")));
 

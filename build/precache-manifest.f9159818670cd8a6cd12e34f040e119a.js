f9994c5668c326fd727641672f00d74fd38c34da
mohammad esmaiel Emadi <mohesem@gmail.com>
2020-04-25 19:21:02 +0430

    with express

---
+++ build/precache-manifest.f9159818670cd8a6cd12e34f040e119a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d65c6e3a9d4e61c1be3239e99a40804d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db7b2a315d8d6d4d94c8",
+    "url": "/static/css/2.d6e02de8.chunk.css"
+  },
+  {
+    "revision": "80d98b843d49d4f49467",
+    "url": "/static/css/main.2d90fbb8.chunk.css"
+  },
+  {
+    "revision": "db7b2a315d8d6d4d94c8",
+    "url": "/static/js/2.d95bc5f6.chunk.js"
+  },
+  {
+    "revision": "80d98b843d49d4f49467",
+    "url": "/static/js/main.1725a205.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "17d273aed6b4fee38cfbf791fc3847c3",
+    "url": "/static/media/IRANSans.17d273ae.woff"
+  },
+  {
+    "revision": "bfc688ca490f0079051aea5b8ec75cbe",
+    "url": "/static/media/ayo-ogunseinde-2.bfc688ca.jpg"
+  },
+  {
+    "revision": "bb4b9ac9fd07e48f86c719495b65341c",
+    "url": "/static/media/clem-onojeghuo-2.bb4b9ac9.jpg"
+  },
+  {
+    "revision": "a6f24e7dd5470a8bd2f48ebb53dbe8d5",
+    "url": "/static/media/damir-bosnjak.a6f24e7d.jpg"
+  },
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/static/media/joe-gardner-2.76ff47ae.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "aab414f7b69d0d4ad897e75e735f240a",
+    "url": "/static/media/mike.aab414f7.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/static/media/nucleo-icons.f32ed5ad.eot"
+  }
+]);

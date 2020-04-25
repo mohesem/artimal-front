84aa8a19167e8698597c388c404a818bfd85f939
mohammad esmaiel Emadi <mohesem@gmail.com>
2020-04-25 17:49:36 +0430

    with express

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 import express from "express";
+
 import Cors from "cors";
 import path from "path";
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.resolve("build")));
 app.use(Cors());
 
-app.get("/app", (req, res) => {
+app.get("/", (req, res) => {
   console.log("got the req");
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.resolve("build", "index.html"));
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
